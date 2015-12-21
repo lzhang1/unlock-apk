38aab5548c86e376a649d058e93a0a1cf7693a54
xudafeng <xudafeng@126.com>
2015-12-22 00:13:49 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# unlock-apk
+
+[![NPM version][npm-image]][npm-url]
+[![build status][travis-image]][travis-url]
+[![Test coverage][coveralls-image]][coveralls-url]
+[![node version][node-image]][node-url]
+[![npm download][download-image]][download-url]
+
+[npm-image]: https://img.shields.io/npm/v/unlock-apk.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/unlock-apk
+[travis-image]: https://img.shields.io/travis/xudafeng/unlock-apk.svg?style=flat-square
+[travis-url]: https://travis-ci.org/xudafeng/unlock-apk
+[coveralls-image]: https://img.shields.io/coveralls/xudafeng/unlock-apk.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/xudafeng/unlock-apk?branch=master
+[node-image]: https://img.shields.io/badge/node.js-%3E=_0.10-green.svg?style=flat-square
+[node-url]: http://nodejs.org/download/
+[download-image]: https://img.shields.io/npm/dm/unlock-apk.svg?style=flat-square
+[download-url]: https://npmjs.org/package/unlock-apk
+
+> unlock-apk
+
+## Installment
+
+```bash
+$ npm i unlock-apk --save
+```
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 xdf
---
+++ script/build.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+/* ================================================================
+ * unlock-apk by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Wed Aug 26 2015 11:55:14 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+var path = require('path');
+var _ = require('../lib/helper');
+var spawn = require('win-spawn');
+//var Build = require('java-build');
+var JAVA_HOME = require('java-home');
+
+var isWindows = _.platform.isWindows;
+var cwd = path.join(__dirname, '..');
+
+JAVA_HOME.getPath(function(error, javaHome) {
+  if (error) {
+    throw 'JAVA_HOME is not set';
+  }
+  console.log('JAVA_HOME is set to ' + javaHome);
+
+  var env = global.process.env;
+
+  if (!env.ANDROID_HOME) {
+    throw 'ANDROID_HOME is not set';
+  }
+
+  var cmd = path.join(__dirname, 'ant', 'bin', 'ant');
+
+  if (isWindows) {
+    cmd = cmd + '.bat';
+  }
+
+  var process = spawn(cmd, ['debug'], {
+    cwd: cwd
+  });
+  process.on('error', function(err) {
+    throw err;
+  });
+
+  process.stdout.setEncoding('utf8');
+  process.stderr.setEncoding('utf8');
+
+  process.stdout.on('data', function(data) {
+    console.log(data);
+  });
+  process.stderr.on('data', function(data) {
+    console.log(data);
+  });
+
+  process.on('exit', function(code) {
+    if (code !== 0) {
+      throw 'build failed';
+    }
+    callback();
+  });
+
+});
